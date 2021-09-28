59a81ca5f8b6238a2372f2bf3ab1bd0629f0b518
tamjeedhur <tamjeedhur@gmail.com>
2021-09-29 01:11:19 +0500

    with some changes made

--- components/SeenSection/index.jsx
+++ components/SeenSection/index.jsx
@@ -21,28 +21,37 @@ const SeenSection = () => {
           <Grid container spacing={2}>
             <Grid item xs={12} md={3}>
               <div className={styles.imgSeenOuter}>
-                <a href="https://bscarmy.com/">
+                <a href="https://bscarmy.com/" target="_blank">
                   <Image src={Logo1} alt="Picture of the logo1" />
                 </a>
               </div>
             </Grid>
             <Grid item xs={12} md={3}>
               <div className={styles.imgSeenOuter}>
-                <a href="https://finance.yahoo.com/news/cd3d-where-defi-gaming-meet-012900190.html">
+                <a
+                  href="https://finance.yahoo.com/news/cd3d-where-defi-gaming-meet-012900190.html"
+                  target="_blank"
+                >
                   <Image src={Logo2} alt="Picture of the logo2" />
                 </a>
               </div>
             </Grid>
             <Grid item xs={12} md={3}>
               <div className={styles.imgSeenOuter}>
-                <a href="https://www.marketwatch.com/press-release/cd3d-where-defi-and-gaming-meet-set-to-launch-in-october-2021-09-23">
+                <a
+                  href="https://www.marketwatch.com/press-release/cd3d-where-defi-and-gaming-meet-set-to-launch-in-october-2021-09-23"
+                  target="_blank"
+                >
                   <Image src={Logo3} alt="Picture of the logo3" />
                 </a>
               </div>
             </Grid>
             <Grid item xs={12} md={3}>
               <div className={styles.imgSeenOuter}>
-                <a href="https://www.coinspeaker.com/cinemadraft-bsc-cd3d-tokens/">
+                <a
+                  href="https://www.coinspeaker.com/cinemadraft-bsc-cd3d-tokens/"
+                  target="_blank"
+                >
                   <Image src={Logo4} alt="Picture of the logo4" />
                 </a>
               </div>
