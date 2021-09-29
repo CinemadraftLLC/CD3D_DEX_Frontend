17c3821d62c9fa15bf637cea3c00d8d621ad161d
tamjeedhur <tamjeedhur@gmail.com>
2021-09-30 01:28:39 +0500

    react mailchimp

--- components/SubscribeMail/SimpleForm.js
+++ components/SubscribeMail/SimpleForm.js
@@ -7,22 +7,51 @@ const url =
 
 // simplest form (only email)
 // const SimpleForm = () => <MailchimpSubscribe url={url} />;
-const SimpleForm = () => {
+const SimpleForm = ({ status, message, onSubmitted }) => {
   const [email, setEmail] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    email &&
+      email.indexOf("@") > -1 &&
+      onSubmitted({
+        EMAIL: email,
+      });
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className={styles.formContainer}>
+        {status === "sending" && (
+          <div style={{ color: "blue", margin: 0, width: "200px" }}>
+            sending...
+          </div>
+        )}
+        {status === "error" && (
+          <div
+            style={{ color: "red", margin: 0 }}
+            dangerouslySetInnerHTML={{ __html: message }}
+          />
+        )}
+        {status === "success" && (
+          <div style={{ color: "green", margin: 0, width: "200px" }}>
+            Subscribed !
+          </div>
+        )}
         <InputBase
           label="Email"
-          onChange={(E) => setEmail(E.target.value)}
+          onChange={(e) => setEmail(e.target.value)}
           type="email"
           value={email}
           placeholder="Enter email for CD3D updates from CinemaDraft"
           isRequired
         />
 
-        <Button className={styles.formButton} variant="contained">
+        <Button
+          className={styles.formButton}
+          formValues={email}
+          type={"submit"}
+          variant="contained"
+        >
           Subscribe
         </Button>
       </div>
