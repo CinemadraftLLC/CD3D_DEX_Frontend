48ce0b583ac3daea9a5c68c6944578a8c810f6ff
tamjeedhur <tamjeedhur@gmail.com>
2021-10-02 02:59:06 +0500

    without

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
         <CD3DSection />
         <UseSection />
         <SoonSection />
-        {/* <TokeContent /> */}
+        <TokeContent />
         <DraftSection />
         <Timeline />
         <DexSection />
