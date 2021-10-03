2737e5e31c4fd2b7dd2777e214d4b5f9162d1de1
tamjeedhur <tamjeedhur@gmail.com>
2021-10-04 04:16:29 +0500

    curtain page there

--- components/TokenPage/BidPlace/components/BidGrid.jsx
+++ components/TokenPage/BidPlace/components/BidGrid.jsx
@@ -6,38 +6,62 @@ import Typography from "@mui/material/Typography";
 const BidGrid = () => {
   return (
     <div className={styles.bidGridOuter}>
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={4}>
-          <div className={styles.bidGridItem}>
-            <Typography variant="subtitle2" gutterBottom component="p">
-              MINIMUM BID
-            </Typography>
-            <Typography variant="h6" gutterBottom component="h6">
-              $0.01 BUSD/CD3D
-            </Typography>
-          </div>
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <div className={styles.bidGridItem}>
-            <Typography variant="subtitle2" gutterBottom component="p">
-              MINIMUM BUY
-            </Typography>
-            <Typography variant="h6" gutterBottom component="h6">
-              $10.00 BUSD
-            </Typography>
-          </div>
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <div className={styles.bidGridItem}>
-            <Typography variant="subtitle2" gutterBottom component="p">
-              MAXIMUM BUY
-            </Typography>
-            <Typography variant="h6" gutterBottom component="h6">
-              $200,000 BUSD
-            </Typography>
-          </div>
-        </Grid>
-      </Grid>
+      <div className={styles.container}>
+        <div className={styles.bidGridItemSkeleton}>
+          <Typography variant="subtitle2" gutterBottom component="p">
+            MINIMUM BID
+          </Typography>
+          <Typography variant="h6" gutterBottom component="h6">
+            $0.01 BUSD/CD3D
+          </Typography>
+        </div>
+        <div className={styles.bidGridItem}>
+          <Typography variant="subtitle2" gutterBottom component="p">
+            MINIMUM BID
+          </Typography>
+          <Typography variant="h6" gutterBottom component="h6">
+            $0.01 BUSD/CD3D
+          </Typography>
+        </div>
+      </div>
+
+      <div className={styles.container}>
+        <div className={styles.bidGridItemSkeleton}>
+          <Typography variant="subtitle2" gutterBottom component="p">
+            MINIMUM BUY
+          </Typography>
+          <Typography variant="h6" gutterBottom component="h6">
+            $10.00 BUSD
+          </Typography>
+        </div>
+        <div className={styles.bidGridItem}>
+          <Typography variant="subtitle2" gutterBottom component="p">
+            MINIMUM BUY
+          </Typography>
+          <Typography variant="h6" gutterBottom component="h6">
+            $10.00 BUSD
+          </Typography>
+        </div>
+      </div>
+
+      <div className={styles.container}>
+        <div className={styles.bidGridItemSkeleton}>
+          <Typography variant="subtitle2" gutterBottom component="p">
+            MAXIMUM BUY
+          </Typography>
+          <Typography variant="h6" gutterBottom component="h6">
+            $200,000 BUSD
+          </Typography>
+        </div>
+        <div className={styles.bidGridItem}>
+          <Typography variant="subtitle2" gutterBottom component="p">
+            MAXIMUM BUY
+          </Typography>
+          <Typography variant="h6" gutterBottom component="h6">
+            $200,000 BUSD
+          </Typography>
+        </div>
+      </div>
     </div>
   );
 };
