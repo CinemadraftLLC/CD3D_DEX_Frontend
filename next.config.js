1eeda76df7f65d7722e6e609f1699aea023be9b7
umeda0213 <umeda.sr.ibri@gmail.com>
2021-12-08 16:34:20 +0800

    2021-12-08

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const withImages = require("next-images");
 
 module.exports = {
   withImages,
+  images: {
+    loader: 'static'
+  },
   env: {
     REACT_APP_NETWORK_URL: process.env.REACT_APP_NETWORK_URL,
     REACT_APP_CHAIN_ID: process.env.REACT_APP_CHAIN_ID
