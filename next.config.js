f33b57c295d0d95366cdba11b55caedf70a83d25
Hosokawa Zen <56865797+hosokawa-zen@users.noreply.github.com>
2021-12-09 12:33:54 +0800

    fix deploy

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 const withImages = require("next-images");
 
 module.exports = {
-  images: {
-    domains: ['localhost', 'http://18.116.235.55'],
-  },
+  withImages,
   env: {
     REACT_APP_NETWORK_URL: process.env.REACT_APP_NETWORK_URL,
     REACT_APP_CHAIN_ID: process.env.REACT_APP_CHAIN_ID
   }
-};
+}
