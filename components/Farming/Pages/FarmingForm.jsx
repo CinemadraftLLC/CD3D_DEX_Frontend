57cf7cfcff42e9df04d3a02d0cb0202046c15b9f
Akioalex <55016644+umeda0213@users.noreply.github.com>
2021-12-13 13:06:44 +0800

    2021-12-13

--- components/Farming/Pages/FarmingForm.jsx
+++ components/Farming/Pages/FarmingForm.jsx
@@ -7,23 +7,16 @@ const FarmingForm = (props) => {
     const {onStack} = props;
 
     return (
-        // <div className={styles.farmingFormContainer}>
-        //     <div className={styles.form_wrapper}>
-        //         <FarmingItem farm={{}} onStack={onStack}/>
-        //         <FarmingItem farm={{}} onStack={onStack}/>
-        //         <FarmingItem farm={{}} onStack={onStack}/>
-        //     </div>
-        // </div>
         <Container maxWidth={"xl"}>
             <Box className={styles.farmingFormContainer}>
-                <Grid container spacing={{xs: 2, md: 3}} className={styles.form_wrapper}>
-                    <Grid item xs={12} sm={6} md={4} xl={6}>
+                <Grid container className={styles.form_wrapper}>
+                    <Grid item xs={12} sm={12} md={6} xl={4} >
                         <FarmingItem farm={{}} onStack={onStack}/>
                     </Grid>
-                    <Grid item xs={12} sm={6} md={4} xl={6}>
+                    <Grid item xs={12} sm={12} md={6} xl={4}>
                         <FarmingItem farm={{}} onStack={onStack}/>
                     </Grid>
-                    <Grid item xs={12} sm={6} md={6} xl={6}>
+                    <Grid item xs={12} sm={12} md={6} xl={4}>
                         <FarmingItem farm={{}} onStack={onStack}/>
                     </Grid>
                 </Grid>
