abb7507e0a2962e6a4838fc1e016df9af49f6e2e
Akioalex <55016644+umeda0213@users.noreply.github.com>
2021-12-27 11:33:59 +0800

    2021-12-27

--- components/Swap/SwapEndAdornment.js
+++ components/Swap/SwapEndAdornment.js
@@ -1,11 +1,22 @@
 import React from "react";
 import {Typography} from "@material-ui/core";
-import {Box, Stack} from "@mui/material";
+import {Box, Chip, Stack} from "@mui/material";
 import Image from "next/image";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import {styled} from "@mui/material/styles";
+
+const SwapMaxChip = styled(Chip)({
+    backgroundColor: "#800022",
+    color: "#FFF1F5",
+    borderRadius: "6px",
+    marginRight: "7px",
+    '&:hover': {
+        backgroundColor: "#E5234A",
+    }
+});
 
 const SwapEndAdornment = (props) => {
-    const {value, onClick} = props;
+    const {value, onClick, onMaxClick} = props;
 
     const getImage = (val) => {
         let result = "/assets/images/cd3d.png";
@@ -24,12 +35,15 @@ const SwapEndAdornment = (props) => {
     }
 
     return (
-        <Stack direction={"row"} alignItems={"center"} style={{cursor: "pointer"}} onClick={() => onClick()}>
-            <Typography component={'span'} variant="subtitle1">{value?.symbol}</Typography>
-            <Box sx={{width: "22px", height: "22px", marginLeft: "7px"}}>
+        <Stack direction={"row"} alignItems={"center"} style={{cursor: "pointer"}}>
+            <SwapMaxChip size="small" label={"Max"} onClick={() => onMaxClick()}/>
+            <Box sx={{minWidth: "42px"}} onClick={() => onClick()}>
+                <Typography component={'span'} variant="subtitle1">{value?.symbol}</Typography>
+            </Box>
+            <Box sx={{width: "22px", height: "22px", marginLeft: "7px"}} onClick={() => onClick()}>
                 <Image src={getImage(value)} width={22} height={22} objectFit={"contain"}/>
             </Box>
-            <ArrowDropDownIcon fontSize={"small"} sx={{color: "white"}}/>
+            <ArrowDropDownIcon fontSize={"small"} sx={{color: "white"}} onClick={() => onClick()}/>
         </Stack>
     );
 
