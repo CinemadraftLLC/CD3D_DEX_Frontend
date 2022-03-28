eab4318df2e9ce3452d9a04271fbd3314d15abaa
Shubham Batra <mrshubhambatra@gmail.com>
2022-03-28 21:04:10 +0530

    reamoved

--- services/services.js
+++
@@ -1,9 +0,0 @@
-import api from "../utils/axios";
-
-export const getData = async () => {
-  return await api.get("data/?environment=mainnet");
-};
-
-export const submitBid = async (data) => {
-  return await api.post("bid/?environment=mainnet", data);
-};
--- utils/axios.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "https://api.crystalelephant.net/api/",
-});
-
-export default api;
