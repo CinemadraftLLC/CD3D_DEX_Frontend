cffb737e7cf16a767ab16a854288ab183d6c41f5
top1st <riguang102@gmail.com>
2022-05-06 02:56:28 +0800

    staking page update select mining type

---
+++ pages/stacking/st_staking_icon.jsx
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+const SvgComponent = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} {...props}>
+    <path
+      data-name="Single Token Staking Icon"
+      d="M12.063 24a.6.6 0 0 0 .347-.161l11.426-11.428a.574.574 0 0 0 .164-.41A12.027 12.027 0 0 1 12.063 24Zm-.125 0A12.034 12.034 0 0 1 .001 12.033a.57.57 0 0 0 .169.375l11.42 11.428a.6.6 0 0 0 .348.161ZM.003 11.935A12.034 12.034 0 0 1 11.967 0a.584.584 0 0 0-.375.169L.172 11.594a.567.567 0 0 0-.174.341Zm23.994 0a.572.572 0 0 0-.161-.34L12.41.17A.574.574 0 0 0 12 .001a12.026 12.026 0 0 1 12 11.934Z"
+      fill="currentColor"
+    />
+  </svg>
+)
+
+export default SvgComponent
